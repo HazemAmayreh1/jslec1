eba9c12fc29ef69a3b497385e969b4cb66a3fcf2
Hazem agil <21099@ppu.edu.ps>
2024-01-02 17:01:51 +0200

    firsttestjs

---
+++ main.js
@@ -0,0 +1,6 @@
+alert("welcommmmmmmmmmmmmmmmmmm");
+document.querySelector(".test h1").textContent="welcome hello";
+document.querySelector(".test h1").classList.add("demo");
+document.querySelector(".testvip h1").textContent="Vip class";
+document.querySelector(".testvip h1").classList.add("vip");
+document.querySelector(".testvip h1").classList.remove("vip");
